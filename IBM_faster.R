f6984548bf0975e9b7644c460b49ac66e73e09bd
mikeroswell <mike.roswell@gmail.com>
2024-09-10 09:56:01 -0400

    Autocommit about to figure out a smarter way to get event times and order, it's currently both taking a long time and possibly wrong.

--- IBM_faster.R
+++ IBM_faster.R
@@ -6,7 +6,7 @@
 # library(GillespieSSA2)
 library(dplyr)
 # library(purrr)
-library(tidyr)
+# library(tidyr)
 # library(ggplot2)
 # library(patchwork) # for assembling ggplot objects
 
@@ -20,34 +20,40 @@ library(tidyr)
 # we will assume that recovered individuals cannot be reinfected.
 
 # first, play with this mixing idea
-popSize <- 5 #2e3
+popSize <- 2e2
 # some shape parameter
 mixKap <- 2.2
-mixMean <- 3 # I think we can set the mean per capita mix rate here
+mixScale <- 1 # Too hard to think about this so leave at default to rescale later
 # mixing propensity
 mixProp <- qgamma(p = ((1:popSize) - 1/2)/popSize, shape = 1/mixKap
-                  , scale = mixMean*mixKap)
-rateInds <-combn(1:popSize, 2) 
-rateFrame <- rateInds |> 
-  rbind(rateInds |> 
-          apply(MARGIN = 2, FUN = function(x){ mixProp[x[1]]*mixProp[x[2]]}) ) |> 
-  t() |> 
-  data.frame() 
+                  , scale = mixScale)
+rateInds <-combn(1:popSize, 2)
+dailyRate <- 3
+
+rateFrame <- rateInds |>
+  rbind(rateInds |>
+          apply(MARGIN = 2, FUN = function(x){ mixProp[x[1]]*mixProp[x[2]]}) ) |>
+  t() |>
+  data.frame()
 names(rateFrame)<- c("ind1", "ind2", "mixRate")
+rateFrame <- rateFrame |>
+  mutate(mixRate = dailyRate * popSize * mixRate / sum(mixRate))
 
 # get event timings: here is when each individual contact occurs
 # Initialization
-tCur <- 0 
+tCur <- 0
 i <- 0 # meaningless counter
-tMax <- 2e3
+tMax <- 2e2
 Who <- rep(0L, popSize*mixMean*tMax)
 When <- rep(0, popSize*mixMean*tMax)
 # make and sort events
 
 
+# I think I can do this smarter
+
 contactInds <- while(tCur < tMax){
   i <- i + 1
-  ev <- rexp(1:length(rateFrame$mixRate), rate = rateFrame$mixRate) 
+  ev <- rexp(1:length(rateFrame$mixRate), rate = rateFrame$mixRate)
   tE <- min(ev)
   tCur <- tE +tCur
   When[i] <- tCur
@@ -60,26 +66,26 @@ contactOrder <- cbind(t(rateInds)[ Who,], When)
 
 # We will also pre-compute recovery times
 # First lets assume it is exponential to compare to basic model
-setGamma <- 0.1 # 
+setGamma <- 0.1 #
 recDelay <- rexp(1:popSize, rate = setGamma)
-iTime <- rep(0, popSize)
+iTime <- rep(tMax, popSize)
 # first, check with SIR assumption
 # mat <- outer(rep(1, popSize), rep(1, popSize))
 # # seems good and kappa for secondary cases is nearly 1
 # contactProb <- 2*mat/sum(mat)
-# contactProb[lower.tri(contactProb, diag = TRUE)] <- 0 
+# contactProb[lower.tri(contactProb, diag = TRUE)] <- 0
 
 # epidemic parameters
 
 
 
 tProb <- 0.1 # transmission event in 10% of effective contacts
-setBeta <- mixMean*tProb
+setBeta <- dailyRate*tProb
 
 
 
 # we're getting towards the simulation now
-Sstate <- 1 
+Sstate <- 1
 Istate <- 2
 Rstate <- 3
 
@@ -87,51 +93,67 @@ states <- rep(Sstate, popSize)
 # initialize with one random infection
 p0 <- sample(1:popSize, 1)
 states[p0]<- Istate
-tCur <- 0 
+tCur <- 0
 iTime[p0] <- tCur
 
 # keep track of cases per person
 
-caseTally <- rep(0, popSize)
+caseTally <- rep(0L, popSize)
 
-# steps 
-i <- 0
-totalContacts <- 0
+# Days
+dayz <- 0L
+# totalContacts <- 0
 
-I <- rep(0L, popSize)
+I <- sum(states == Istate)
+S <- sum(states == Sstate)
 
-
-#### 
+####
+# Think about if this gets params as inputs too.
+atEachStep <- function(co){
 # a bunch of stuff I want to do for each row...
   # check if anyone is infectious
-  if(any(states[contactOrder[,1:2]]) == Istate){
+  tCur <- co[3]
+  if(any(states[co[1:2]] == Istate)){
         # see if anyone has already recovered
-    if(iTime + )
-    caseTally[myContact[states[c(myContact)] == 2]] <- caseTally[myContact[states[c(myContact)] == 2]] +
-      newI
-    if(newI ==1){states[c(myContact)] <- 2}
+    recVec <- (iTime[co[1:2]] + recDelay[co[1:2]]) <= tCur
+    # count them
+    I <- I - sum(recVec)
+    # remove them
+    states[co[1:2]][recVec] <- Rstate
+    # if one is infectious AND one is susceptible, lots to do
+    if(sum(states[co[1:2]]) == Istate+Sstate){
+      # first flip the coin
+     if(rbinom(1,1, prob = tProb)){
+       # update cumulative state counters
+        I <- I + 1
+        S <- S - 1
+        # count the win
+        caseTally[co[1:2][states[co[1:2]] == 2]] <- caseTally[co[1:2][states[co[1:2]] == 2]] + 1
+        # record the infection time
+        iTime[co[1:2][states[co[1:2]] == 1]] <- tCur
+        # update the states
+        states[co[1:2][states[co[1:2]] == 1]] <- Istate
+
     }
   }
-# if not, then somebody recovers
-  else{
-  tCur <- tCur +  waitRecovery
-  states[sample((1:popSize)[states ==2], 1)] <- 3
-  }
-# would like to print this (or something) every large jump instead of every event, don't know how
-  cat(paste0("t = ", tCur, ",  I = ", I, ", maxCases = "
+}
+# [save state? and] print some stuff once per day
+  if(floor(tCur>dayz)){
+      dayz <- dayz + 1
+      cat(paste0("t = ", tCur, ",  I = ", I, ", S = ", S, ", maxCases = "
       , max(caseTally)
-      , ", per capita contacts = "
-      , totalContacts/(popSize * tCur)
       , "\n")
-  )
-  # make sure to stop when no new infections
-  if(sum(states == 1)==0| sum(states ==2)==0){break}
+    )
+    }
+  # stop when no new infections
+ # if(S == 0 | I == 0){stop}
 }
 
+apply(contactOrder, 1, atEachStep)
 
 # check Kappa
 kd <- function(x){(sd(x)^2-mean(x))/mean(x)^2}
-kd(caseTally[states == 3]) # slightly higher than 1 with this setup. 
+kd(caseTally[states == 3]) # slightly higher than 1 with this setup.
 
 
 
